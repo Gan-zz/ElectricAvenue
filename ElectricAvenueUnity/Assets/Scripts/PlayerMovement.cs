7d90fded366c5d722d6c1d6029456ebf40284a39
Gan-zz <2377487@fife.ac.uk>
2026-05-31 17:30:47 +0100

    top down movement
    added top down movement, haven't figured out how to stop player when moving

--- ElectricAvenueUnity/Assets/Scripts/PlayerMovement.cs
+++ ElectricAvenueUnity/Assets/Scripts/PlayerMovement.cs
@@ -2,21 +2,46 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 1f;
+    public float speed = 1f; //horizontal movement
+    public float verticalSpeed = 1f; //vertical movement
     private Rigidbody2D physicsBody = null;
 
     private void Awake()
     {
         physicsBody = GetComponent<Rigidbody2D>();
     }
-    
+    //moves left
     public void MoveLeft()
     {
         physicsBody.linearVelocityX = -speed;
     }
-
+    //moves right
     public void MoveRight()
     {
         physicsBody.linearVelocityX = speed;
     }
+    //moves upward
+    public void MoveUp()
+    {
+        physicsBody.linearVelocityY = verticalSpeed;
+    }
+
+    //moves downward
+    public void MoveDown()
+    {
+        physicsBody.linearVelocityY = -verticalSpeed;
+    }
+
+    //stops horizontal movement
+    public void StopHorizontalMovement()
+    {
+        physicsBody.linearVelocityX = 0f;
+    }
+
+    //stops vertical movement
+    public void StopVerticalMovement()
+    {
+        physicsBody.linearVelocityY = 0f;
+    }
+
 }
