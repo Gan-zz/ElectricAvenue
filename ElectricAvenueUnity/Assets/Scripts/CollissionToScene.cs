ac6ab2f23e4b321e9dfccb33e512b1ca8d10fb1b
Gan-zz <2377487@fife.ac.uk>
2026-06-01 12:33:57 +0100

    added change to scene based on collions
    scene changes

---
+++ ElectricAvenueUnity/Assets/Scripts/CollissionToScene.cs
@@ -0,0 +1,28 @@
+using Unity.VisualScripting;
+
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class Collision : MonoBehaviour
+
+{
+
+    private void OnCollisionEnter2D(Collision2D collision)
+
+    {
+
+        //code to handle collision
+
+        //Get the collider of the object we collided with
+
+        Collider2D collider = collision.collider;
+
+        SceneManager.LoadScene("EndScene");
+
+
+
+    }
+
+}
+
