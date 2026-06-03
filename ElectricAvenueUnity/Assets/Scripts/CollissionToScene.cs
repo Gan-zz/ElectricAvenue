7bb762df94cec4e31ff976a0a5b6c9a118845f7b
Gan-zz <2377487@fife.ac.uk>
2026-06-03 12:34:47 +0100

    added a timer

--- ElectricAvenueUnity/Assets/Scripts/CollissionToScene.cs
+++ ElectricAvenueUnity/Assets/Scripts/CollissionToScene.cs
@@ -1,28 +1,15 @@
 using Unity.VisualScripting;
-
 using UnityEngine;
-
 using UnityEngine.SceneManagement;
 
 public class Collision : MonoBehaviour
-
 {
-
     private void OnCollisionEnter2D(Collision2D collision)
-
     {
-
         //code to handle collision
-
         //Get the collider of the object we collided with
-
         Collider2D collider = collision.collider;
-
         SceneManager.LoadScene("EndScene");
-
-
-
     }
-
 }
 
---
+++ ElectricAvenueUnity/Assets/Scripts/Timer.cs
@@ -0,0 +1,23 @@
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] float elapsedTime;
+    void Update()
+    {
+        //if the Time is above 0
+        if (elapsedTime >= 0)
+        {
+            elapsedTime += Time.deltaTime;
+        }
+        
+        int minutes = Mathf.FloorToInt(elapsedTime / 60);
+        int seconds = Mathf.FloorToInt(elapsedTime % 60);
+        //Changes the text of the timerText to the Time
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
