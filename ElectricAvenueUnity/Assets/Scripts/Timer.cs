45a158cbbe4172b65485c8e3f061fe1efadf330e
Gan-zz <2377487@fife.ac.uk>
2026-06-07 14:23:04 +0100

    added comments to better explain how timer code works

--- ElectricAvenueUnity/Assets/Scripts/Timer.cs
+++ ElectricAvenueUnity/Assets/Scripts/Timer.cs
@@ -1,23 +1,28 @@
-using TMPro;
+using TMPro; 
 using Unity.VisualScripting;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine; 
+using UnityEngine.SceneManagement; 
 
 public class Timer : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI timerText;
     [SerializeField] float elapsedTime;
+
     void Update()
     {
         //if the Time is above or equal to 0
+
         if (elapsedTime >= 0)
         {
+            //increade the elapsed time by the time passed since the last frame
+            //Time.deltaTime makes sure the timer updates smoothly regardless of frame rate
             elapsedTime += Time.deltaTime;
         }
-        
-        int minutes = Mathf.FloorToInt(elapsedTime / 60);
-        int seconds = Mathf.FloorToInt(elapsedTime % 60);
-        //Changes the text of the timerText to the Time
+
+        //convert the elapsed time into minutes and seconds
+        //Mathf.FloorToInt rounds down to the nearest integer
+        int minutes = Mathf.FloorToInt(elapsedTime / 60); //total minutes passed
+        int seconds = Mathf.FloorToInt(elapsedTime % 60); //remaining seconds after accounting for minutes
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 }
